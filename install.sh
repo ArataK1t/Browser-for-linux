e02cb61bbaf804fc87bb60778c6bfd1494f5ee17
ArataK1t <168479108+ArataK1t@users.noreply.github.com>
2024-12-08 20:41:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -135,7 +135,7 @@ fi
 
 # Функция для получения случайного User-Agent (только десктопные)
 generate_random_config() {
-  user_agent=$(python3 -c 'from fake_useragent import UserAgent; ua = UserAgent(platforms=["windows", "mac", "linux"], use_cache=True); print(ua.random)')
+  user_agent=$(python3 -c 'from fake_useragent import UserAgent; ua = UserAgent(platforms=["windows", "mac", "linux"]); print(ua.random)')
 
   # Определение языка и таймзоны на основе прокси
   proxy_ip="$1"
